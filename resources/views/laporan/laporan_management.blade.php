e775af11fc1e6a0052d6632650042eb229a1a751
lanolunip <michaelwilbert11@gmail.com>
2021-01-30 15:06:34 +0700

    merubah link edit menjadi view di laporan

--- resources/views/laporan/laporan_management.blade.php
+++ resources/views/laporan/laporan_management.blade.php
@@ -29,7 +29,7 @@
                         <td>{{ $i }}</td>
                         <td>{{ $l->isi }}</td>
                         <td>{{ $l->Penugasan->banyak_pengeluaran }}</td>
-                        <td><a href="/penugasan/edit/{{$l->Penugasan->id}}"> {{ $l->Penugasan->nama }}</a></td>
+                        <td><a href="/penugasan/view/{{$l->Penugasan->id}}"> {{ $l->Penugasan->nama }}</a></td>
                         <td>
                             <a href="/laporan/edit/{{ $l->id }}" class="btn btn-warning">Edit</a>
                             <a href="/laporan/hapus/{{ $l->id }}" class="btn btn-danger">Hapus</a>
